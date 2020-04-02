45ce0a3a0afd524748af9fa3b486e254c32f49bc
yamagami2211 <yamagami2211@svk.jp>
2020-04-02 22:57:27 +0900

    issues #4 開発中

--- src/main/java/biz/yama2211/opengui/Main.java
+++ src/main/java/biz/yama2211/opengui/Main.java
@@ -1,128 +1,168 @@
 package biz.yama2211.opengui;
 
 import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.EnchantingInventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Dye;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener
-{
+public class Main extends JavaPlugin implements Listener {
 	//
-public void onEnable(){
-	getServer().getPluginManager().registerEvents(this, this);
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
 
-}
+	}
 
-public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-	if (!(sender instanceof Player)) {
-		sender.sendMessage(ChatColor.RED + "このコマンドはゲーム内から実行してください。");
-	} else {
-		Player player = (Player)sender;
-	//エンダーチェストを開く
-	if (cmd.getName().equalsIgnoreCase("enderchest")) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED + "このコマンドはゲーム内から実行してください。");
+			saveDefaultConfig();
+		} else {
+			Player player = (Player) sender;
+			//エンダーチェストを開く
+			if (cmd.getName().equalsIgnoreCase("enderchest")) {
 
-		if (args.length == 0) {
- {
-			if ((sender.hasPermission("opengui.enderchest")) || (sender.isOp())) { //Pex1
+				if (args.length == 0) {
+					{
+						if ((sender.hasPermission("opengui.enderchest")) || (sender.isOp())) { //Pex1
 
-			player.openInventory(player.getEnderChest());
-			sender.sendMessage(ChatColor.GREEN + "エンダーチェストを開きました" );
-			}//Pex1
-			else {
-				sender.sendMessage(ChatColor.RED + "権限がありません。");
+							player.openInventory(player.getEnderChest());
+							sender.sendMessage(ChatColor.GREEN + "エンダーチェストを開きました");
+						} //Pex1
+						else {
+							sender.sendMessage(ChatColor.RED + "権限がありません。");
+						}
+					}
+				}
+				//(command) <PlayerName> で<PlayerName>のエンダーチェストを開く
+				if (args.length == 1) {
+					if ((sender.hasPermission("opengui.enderchest.other")) || (sender.isOp())) { //Pex1.5
+						try {
+							Player targetplayer = player.getServer().getPlayer(args[0]);
+
+							player.openInventory(targetplayer.getEnderChest());
+							sender.sendMessage(
+									ChatColor.GOLD + targetplayer.getName() + ChatColor.GREEN + " のエンダーチェストを開きました");
+
+						} catch (Exception err) {
+							player.sendMessage(ChatColor.GOLD + args[0] + ChatColor.RED + " はオフラインです。");
+						}
+
+					} //Pex1.5
+					else {
+						sender.sendMessage(ChatColor.RED + "権限がありません。");
+					}
+				}
+
 			}
-	}
-		}
-	//(command) <PlayerName> で<PlayerName>のエンダーチェストを開く
-	if (args.length == 1)
-	{
-		if ((sender.hasPermission("opengui.enderchest.other")) || (sender.isOp())) { //Pex1.5
-	    try
-	    {
-	        Player targetplayer = player.getServer().getPlayer(args[0]);
+			//インベントリを開く
+			if (cmd.getName().equalsIgnoreCase("inventory")) {
 
-	        player.openInventory(targetplayer.getEnderChest());
-	        //player.openInventory(targetplayer.getEnderChest());
-	        sender.sendMessage(ChatColor.GOLD + targetplayer.getName()+ ChatColor.GREEN + " のエンダーチェストを開きました" );
+				if (args.length == 0) {
+					if ((sender.hasPermission("opengui.inventory")) || (sender.isOp())) { //Pex2
+						player.openInventory(player.getInventory());
+						sender.sendMessage(ChatColor.GREEN + "インベントリを開きました");
+					} //pex2
+					else {
+						sender.sendMessage(ChatColor.RED + "権限がありません。");
+					}
+				}
+				//(command) <PlayerName> で<PlayerName>のインベントリを開く
+				if (args.length == 1) {
+					if ((sender.hasPermission("opengui.inventory.other")) || (sender.isOp())) { //Pex2.5
+						try {
+							Player targetplayer = player.getServer().getPlayer(args[0]);
 
-	    }
-	    catch (Exception err)
-	    {
-	      player.sendMessage(ChatColor.GOLD + args[0] + ChatColor.RED + " はオフラインです。");
-	    }
+							player.openInventory(targetplayer.getInventory());
+							sender.sendMessage(
+									ChatColor.GOLD + targetplayer.getName() + ChatColor.GREEN + "のインベントリを開きました");
 
-	}//Pex1.5
-		else {
-			sender.sendMessage(ChatColor.RED + "権限がありません。");
-		}
-		}
+						} catch (Exception err) {
+							player.sendMessage(ChatColor.GOLD + args[0] + ChatColor.RED + " はオフラインです。");
+						}
+					} //pex2.5
+					else {
+						sender.sendMessage(ChatColor.RED + "権限がありません。");
+					}
 
-	}
-    	//インベントリを開く
-	if(cmd.getName().equalsIgnoreCase("inventory")) {
+				}
 
-		if(args.length == 0) {
-			if ((sender.hasPermission("opengui.inventory")) || (sender.isOp())) { //Pex2
-			player.openInventory(player.getInventory());
-			sender.sendMessage(ChatColor.GREEN +"インベントリを開きました" );
-		} //pex2
-			else {
-				sender.sendMessage(ChatColor.RED + "権限がありません。");
 			}
-	}
-		//(command) <PlayerName> で<PlayerName>のインベントリを開く
-	if (args.length == 1)
-	{
-		if ((sender.hasPermission("opengui.inventory.other")) || (sender.isOp())) { //Pex2.5
-	    try
-	    {
-	        Player targetplayer = player.getServer().getPlayer(args[0]);
 
-	        player.openInventory(targetplayer.getInventory());
-	        sender.sendMessage(ChatColor.GOLD +targetplayer.getName() + ChatColor.GREEN + "のインベントリを開きました");
+			//ワークベンチ(作業台)を開く
+			if (cmd.getName().equalsIgnoreCase("workbench")) {
 
-	    }
-	    catch (Exception err)
-	    {
-	      player.sendMessage(ChatColor.GOLD + args[0] + ChatColor.RED + " はオフラインです。");
-	    }
-	}//pex2.5
-		else {
-			sender.sendMessage(ChatColor.RED + "権限がありません。");
+				if ((sender.hasPermission("opengui.workbench")) || (sender.isOp())) { //Pex3
+					player.openWorkbench(null, true);
+					sender.sendMessage(ChatColor.GREEN + "ワークベンチを開きました");
+				} //Pex3
+				else {
+					sender.sendMessage(ChatColor.RED + "権限がありません。");
+				}
+			}
+			//エンチャントテーブルを開く (ラピスラズリ必須)
+			if (cmd.getName().equalsIgnoreCase("enchanting")) {
+
+				if ((sender.hasPermission("opengui.enchanting")) || (sender.isOp())) { //Pex4
+
+					player.openEnchanting(null, true);
+					sender.sendMessage(ChatColor.GREEN + "エンチャントテーブルを開きました");
+				} //Pex4
+				else {
+					sender.sendMessage(ChatColor.RED + "権限がありません。");
+				}
+			}
+
+		}
+		return false;
+	}//OnCommand
+
+
+	@EventHandler
+	public void onInventoryOpen(InventoryOpenEvent e) {
+		if (e.getInventory() instanceof EnchantingInventory) {
+
+					EnchantingInventory inventory = (EnchantingInventory) e.getInventory();
+
+					Dye dye = new Dye();
+					dye.setColor(DyeColor.BLUE);
+					ItemStack itemStack = dye.toItemStack();
+
+					itemStack.setAmount(64);
+					inventory.setItem(1, itemStack);
 		}
 
-		}
-
 	}
 
-    	//ワークベンチ(作業台)を開く
-	if(cmd.getName().equalsIgnoreCase("workbench")) {
-
-		if ((sender.hasPermission("opengui.workbench")) || (sender.isOp())) { //Pex3
-		player.openWorkbench(null, true);
-		sender.sendMessage(ChatColor.GREEN +"ワークベンチを開きました");
-		}//Pex3
-		else {
-			sender.sendMessage(ChatColor.RED + "権限がありません。");
+	@EventHandler
+	public void onInventoryClose(InventoryCloseEvent e) {
+		if (e.getInventory() instanceof EnchantingInventory) {
+			EnchantingInventory inventory = (EnchantingInventory) e.getInventory();
+			Dye dye = new Dye();
+			dye.setColor(DyeColor.BLUE);
+			ItemStack itemStack = dye.toItemStack();
+			
+			inventory.remove(itemStack);
 		}
 	}
-    	//エンチャントテーブルを開く (ラピスラズリ必須)
-	if(cmd.getName().equalsIgnoreCase("enchanting")) {
-
-		if ((sender.hasPermission("opengui.enchanting")) || (sender.isOp())) { //Pex4
-		player.openEnchanting(null, true);
-		sender.sendMessage(ChatColor.GREEN +"エンチャントテーブルを開きました");
-		}//Pex4
-		else {
-			sender.sendMessage(ChatColor.RED + "権限がありません。");
+	
+	@EventHandler
+	public void onClickInventory(InventoryClickEvent e) {
+		if (e.getInventory() instanceof EnchantingInventory) {
+			EnchantingInventory inventory = (EnchantingInventory) e.getInventory();
+			
 		}
+		
 	}
 
-
-}
-return false;
-}
 }
